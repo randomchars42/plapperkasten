fd7a8ca683a03991da147406434129c1c5474c82
randomchars42 <randomchars42@users.noreply.github.com>
2022-02-06 13:15:31 +0100

    add plugin for auditory feedback

---
+++ src/boxhead/plugins/soundeffects/soundeffects.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+"""Provide auditory feedback.
+"""
+
+import pathlib
+import pkg_resources
+import subprocess
+
+from boxhead import config as boxhead_config
+from boxhead import plugin
+from boxhead.boxheadlogging import boxheadlogging
+
+logger: boxheadlogging.BoxHeadLogger = boxheadlogging.get_logger(__name__)
+
+
+class Soundeffects(plugin.Plugin):
+    """Provide auditory feedback on certain events.
+    """
+
+    def on_init(self, config: boxhead_config.Config) -> None:
+        """Register for events.
+
+        Args:
+            config: The configuration.
+        """
+
+        self.register_for('finished_loading')
+        self.register_for('shutdown')
+        self.register_for('error')
+        self.register_for('feedback')
+        self._path_sounds = pathlib.Path(
+            config.get_str('plugins',
+                           'soundeffect',
+                           'path',
+                           default=pkg_resources.resource_filename(
+                               __name__, 'sounds')))
+        self._sounds: dict[str, str] = config.get_dict_str_str('plugins',
+                                                               'soundeffect',
+                                                               'sounds',
+                                                               default={})
+
+    def on_finished_loading(self, *values: str, **params: str) -> None:
+        # pylint: disable=unused-argument
+        """Play sound to notify the user.
+
+        Args:
+            *values: Values attached to the event (ignored).
+            **params: Parameters attached to the event (ignored).
+        """
+        self.play_sound('ready')
+
+    def on_shutdown(self, *values: str, **params: str) -> None:
+        # pylint: disable=unused-argument
+        """Play sound to notify the user.
+
+        Args:
+            *values: Values attached to the event (ignored).
+            **params: Parameters attached to the event (ignored).
+        """
+        self.play_sound('shutdown')
+
+    def on_error(self, *values: str, **params: str) -> None:
+        # pylint: disable=unused-argument
+        """Play sound to notify the user.
+
+        Args:
+            *values: Values attached to the event (ignored).
+            **params: Parameters attached to the event (ignored).
+        """
+        self.play_sound('error')
+
+    def on_feedback(self, *values: str, **params: str) -> None:
+        # pylint: disable=unused-argument
+        """Play sound to notify the user.
+
+        Args:
+            *values: Values attached to the event (ignored).
+            **params: Parameters attached to the event (ignored).
+        """
+        self.play_sound('feedback')
+
+    def play_sound(self, sound: str):
+        """Play sound to notify the user.
+
+        Args:
+            sound: The name of the sound to play (must be a valid key
+            in plugins.soundeffects.sounds in config.yaml).
+        """
+        if not sound in self._sounds:
+            logger.error('no such sound configured: "%s"', sound)
+            return
+
+        call: list[str] = [
+            '/usr/bin/aplay', '-N',
+            str(self._path_sounds / self._sounds[sound])
+        ]
+        # capture output is new and in this case required with python >= 3.7
+        try:
+            subprocess.run(call,
+                           capture_output=True,
+                           encoding='utf-8',
+                           check=True)
+        except subprocess.CalledProcessError:
+            logger.error('could not play sound "%s"', sound)
