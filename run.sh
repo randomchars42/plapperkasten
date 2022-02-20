734b6badcb30c2df721f7118192202f747125a53
randomchars42 <randomchars42@users.noreply.github.com>
2022-02-20 15:44:47 +0100

    correct paths in setup.cfg and simplify test running

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #/usr/bin/env bash
-
-script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-PYTHONPATH=$PYTHONPATH:"$script_dir"/src python3 -m boxhead.boxhead "$@"
+if command -v pipenv &> /dev/null
+then
+    pipenv run boxhead
+else
+    script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+    PYTHONPATH=$PYTHONPATH:"$script_dir"/src python3 -m boxhead.boxhead "$@"
+fi
