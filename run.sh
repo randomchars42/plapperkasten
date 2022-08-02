7942cdf40a5c0f2f092a8555fb96159bfd12ec84
randomchars42 <randomchars42@users.noreply.github.com>
2022-08-02 19:59:12 +0200

    pass on arguments to the main process under all cirumstances

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 if command -v pipenv &> /dev/null
 then
-    pipenv run boxhead
+    pipenv run boxhead "$@"
 else
     script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
     PYTHONPATH=$PYTHONPATH:"$script_dir"/src python3 -m boxhead.boxhead "$@"
