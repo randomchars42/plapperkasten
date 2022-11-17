randomchars42/plapperkasten

commit f02c725cd12ba8dd2bfb1a988149f63e1f0c242a

fix loading playlists

src/plapperkasten/plugins/mpdclient/mpdclient.py
@@ -197,7 +197,8 @@ class Mpdclient(plugin.Plugin):
 
             if status.key[-4:] == '.m3u':
                 # the key indicates a playlist to be loaded
-                self._mpdclient.load(status.key[:-4])
+                # https://github.com/MusicPlayerDaemon/MPD/issues/200
+                self._mpdclient.load(status.key)
             else:
                 # the key represents a path to a folder the files in which are
                 # to be coerced into a playlist

commit 25766e0cd95e0b790b464673f9e40883821a9112

fix bug introduced by f02c725

src/plapperkasten/plugins/mpdclient/mpdclient.py
@@ -280,7 +280,7 @@ class Mpdclient(plugin.Plugin):
             if key[-4:] == '.m3u':
                 key_list = [
                     'file: ' + file
-                    for file in self._mpdclient.listplaylist(key[:-4])
+                    for file in self._mpdclient.listplaylist(key)
                 ]
             else:
                 key_list = [